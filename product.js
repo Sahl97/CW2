93bcb73a68d3a1816acba34b503a9e67ba9328cc
Sahl97 <149163967+Sahl97@users.noreply.github.com>
2024-02-13 01:03:01 +0400

    Add files via upload

---
+++ product.js
@@ -0,0 +1,112 @@
+let products = [
+    { 
+        "id": 1001,
+        "Subject": "Cricket",
+        "Location": "Dubai",
+        "Price": "100Dhs",
+        "image": "Images/cricket.jpg",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 5
+    },
+    { 
+        "id": 1002,
+        "Subject": "Football",
+        "Location": "Dubai",
+        "Price": "100Dhs",
+        "image": "Images/football.jpg",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 4
+    },
+    { 
+        "id": 1003,
+        "Subject": "Hockey",
+        "Location": "Dubai",
+        "price": "100Dhs",
+        "image": "Images/hockey.jpg",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 3
+    },
+    { 
+        "id": 1004,
+        "Subject": "Swimming",
+        "Location": "Dubai",
+        "price": "100Dhs",
+        "image": "Images/swimming.jpg",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 2
+    },
+    { 
+        "id": 1005,
+        "Subject": "Gymnastics",
+        "Location": "Dubai",
+        "price": "100Dhs",
+        "image": "Images/gymnastics.jpg",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 1
+    },
+    { 
+        "id": 1006,
+        "Subject": "Dance",
+        "Location": "AbuDhabi",
+        "price": "150Dhs",
+        "image": "Images/dance.jpg",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 5
+    },
+    { 
+        "id": 1007,
+        "Subject": "Science",
+        "Location": "AbuDhabi",
+        "price": "200Dhs",
+        "image": "Images/science.png",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 4
+    },
+    { 
+        "id": 1008,
+        "Subject": "Technology",
+        "Location": "AbuDhabi",
+        "price": "$100",
+        "image": "Images/technology.jpg",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 3
+    },
+    { 
+        "id": 1009,
+        "Subject": "English",
+        "Location": "AbuDhabi",
+        "price": "$200",
+        "image": "Images/english.png",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 2
+    },
+    { 
+        "id": 1010,
+        "Subject": "Maths",
+        "Location": "AbuDhabi",
+        "price": "$200",
+        "image": "Images/maths.jpg",
+        "Spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 1
+    },
+]
